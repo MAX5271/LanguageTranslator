f3087ac25c53faddd693dfb682e47b00c78ae5ec
Nitigya Chandel <154728557+MAX5271@users.noreply.github.com>
2024-11-29 12:32:28 +0530

    Update speak.py
    Added comments where ever necessary

--- speak.py
+++ speak.py
@@ -1,13 +1,14 @@
 from gtts import gTTS
 import pygame
 import time
+#importing the required dependences and api(s)
 
-
+#function for text to speech
 def speak(text: str, language: str) -> None:
-    speech = gTTS(text=text, lang=language, slow=False)
+    speech = gTTS(text=text, lang=language, slow=False)#using google text to speeck to create an audio save file
     speech.save("temp.mp3")
     pygame.mixer.init()
-    pygame.mixer.music.load("temp.mp3") 
+    pygame.mixer.music.load("temp.mp3") #playing the audio text to speech
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():  # wait for music to finish playing
         time.sleep(1)
